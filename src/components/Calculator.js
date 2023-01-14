02360282dd5421d0755db72156a5c6f4bc2acae0
Mukama Steven <stevenmukama1@gmail.com>
2023-01-14 13:06:59 +0200

    Use hooks in the magician project

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,57 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from './logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  clickHandler = (e) => {
-    const result = calculate(this.state, e.target.textContent);
-    this.setState((prevState) => ({
+  const clickHandler = (e) => {
+    const result = calculate(state, e.target.textContent);
+    setState((prevState) => ({
       ...prevState,
       ...result,
     }));
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="mainCalc">
-        <div className="firstrowsection">
-          <p className="answertab" type="text">{ next || total || 0}</p>
-          {' '}
-        </div>
-        <div className="secondrow">
-          <button type="button" className="firstsection" onClick={this.clickHandler}>AC</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>+/-</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>%</button>
-          <button type="button" className="secondsection" onClick={this.clickHandler}>÷</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>7</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>8</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>9</button>
-          <button type="button" className="secondsection" onClick={this.clickHandler}>x</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>4</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>5</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>6</button>
-          <button type="button" className="secondsection" onClick={this.clickHandler}>-</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>1</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>2</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>3</button>
-          <button type="button" className="secondsection" onClick={this.clickHandler}>+</button>
-          <button type="button" className="firstsectionzero" onClick={this.clickHandler}>0</button>
-          <button type="button" className="firstsection" onClick={this.clickHandler}>.</button>
-          <button type="button" className="secondsection" onClick={this.clickHandler}>=</button>
-        </div>
+  const { total, next } = state;
+  return (
+    <div className="mainCalc">
+      <div className="firstrowsection">
+        <p className="answertab" type="text">{ next || total || 0}</p>
+        {' '}
       </div>
-    );
-  }
-}
+      <div className="secondrow">
+        <button type="button" className="firstsection" onClick={clickHandler}>AC</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>+/-</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>%</button>
+        <button type="button" className="secondsection" onClick={clickHandler}>÷</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>7</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>8</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>9</button>
+        <button type="button" className="secondsection" onClick={clickHandler}>x</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>4</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>5</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>6</button>
+        <button type="button" className="secondsection" onClick={clickHandler}>-</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>1</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>2</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>3</button>
+        <button type="button" className="secondsection" onClick={clickHandler}>+</button>
+        <button type="button" className="firstsectionzero" onClick={clickHandler}>0</button>
+        <button type="button" className="firstsection" onClick={clickHandler}>.</button>
+        <button type="button" className="secondsection" onClick={clickHandler}>=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
